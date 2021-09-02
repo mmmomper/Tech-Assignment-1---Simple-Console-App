00c77beac17951f15b89e6c1a7d8b82b1488f6bd
Marisa <Marisa@DESKTOP-OSPRCUT>
2021-09-01 22:34:42 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//ISM 4300 Tech Assignment 1 - Simple Console App
+//Submitted by Marisa Momper
+//Creating a console app and connecting to GitHub
+using System;
+
+namespace Tech_Assignment_1___Simple_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Unique message to the console
+            Console.WriteLine("Hi this is Marisa's Console App");
+        }
+    }
+}
